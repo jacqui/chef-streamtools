c2490b8196fb0d7f6a61440dfc477965c40fbbd0
Jacqui Maher <jacqui.maher@nytimes.com>
2014-02-14 11:48:34 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-remote_file "/tmp/streamtools-#{node['streamtools']['version']}.tar.gz" do
+remote_file "/tmp/streamtools-#{node['streamtools']['version']}.gz" do
   source "https://github.com/nytlabs/streamtools/releases/download/#{node['streamtools']['version']}/st-linux-#{node['streamtools']['version']}.gz"
   action :create_if_missing
 end
@@ -11,8 +11,8 @@ end
 bash 'extract_streamtools' do
   cwd '/tmp'
   code <<-EOH
-    tar xzf /tmp/streamtools-#{node['streamtools']['version']}.tar.gz
-    cp /tmp/st-#{node['streamtools']['version']} /usr/local/bin/st
+    gunzip /tmp/st-linux--#{node['streamtools']['version']}.gz
+    cp /tmp/st-linux-#{node['streamtools']['version']} /usr/local/bin/st
     /usr/local/bin/st > /tmp/streamtools.log 2>&1 &
     EOH
 end
