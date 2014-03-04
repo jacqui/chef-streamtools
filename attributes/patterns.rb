168fae39353fcfb64bfca599e58f137472fb537f
Jacqui Maher <jacqui.maher@nytimes.com>
2014-03-04 11:29:48 -0500

    Import patterns into streamtools with this recipe (wip)

---
+++ attributes/patterns.rb
@@ -0,0 +1,4 @@
+default['streamtools']['patterns_repository'] = ''
+default['streamtools']['patterns_branch'] = 'master'
+default['streamtools']['patterns_directory'] = "/home/ubuntu/streamtools-patterns"
+default['streamtools']['pattern_file'] = ''
---
+++ recipes/patterns.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: streamtools
+# Recipe:: patterns
+#
+
+# load pattern from specified repo
+# then notify the http block to post it
+git node['streamtools']['patterns_directory'] do
+  repository node['streamtools']['patterns_repository']
+  revision node['streamtools']['patterns_branch']
+  action :sync
+  user "ubuntu"
+  notifies :run, "bash[post_pattern]"
+  not_if do
+    node['streamtools']['patterns_repository'] == ''
+  end
+end
+
+# post it to local instance of streamtools
+bash "post_pattern" do
+  code <<-EOH
+    curl -XPOST http://localhost:7070/import -d@#{File.join(node['streamtools']['patterns_directory'], node['streamtools']['pattern_file'])}
+  EOH
+  action :nothing
+  only_if do
+    File.exists?(File.join(node['streamtools']['patterns_directory'], node['streamtools']['pattern_file']))
+  end
+end
+
